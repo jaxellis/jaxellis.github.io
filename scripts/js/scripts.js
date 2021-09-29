e27a3582f6142a3f75aee14eda99c1d25688862c
jaxellis <67706810+jaxellis@users.noreply.github.com>
2021-09-28 17:03:06 -0700

    More Content Building

---
+++ scripts/js/scripts.js
@@ -0,0 +1,8 @@
+function ShowDropdown() {
+    var x = document.getElementById("hidden-dropdown");
+    if (x.className.indexOf("show") == -1) {
+      x.className += " show";
+    } else { 
+      x.className = x.className.replace(" show", "");
+    }
+  }
