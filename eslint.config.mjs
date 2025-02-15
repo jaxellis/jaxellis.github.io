d4b6beca5048f26e8ddfbd68bd5123ac0ee2d4d8
jaxellis <67706810+jaxellis@users.noreply.github.com>
2025-02-14 22:32:57 -0800

    Add Prettier eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslint from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslint.configs.recommended,
+
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+
+  prettierConfig,
+
+  {
+    rules: {
+      semi: ['error', 'always'],
+    },
+  },
 ];
 
 export default eslintConfig;
