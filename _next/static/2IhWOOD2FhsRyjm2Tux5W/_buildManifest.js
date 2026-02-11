7c4595dd6184f1ff3da4636901ea07003f44424c
jaxellis <67706810+jaxellis@users.noreply.github.com>
2026-02-10 23:15:03 -0800

    Updates

---
+++ _next/static/2IhWOOD2FhsRyjm2Tux5W/_buildManifest.js
@@ -0,0 +1,22 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/a68fe09e7f3d3608.js"
+  ],
+  "/App": [
+    "static/chunks/5a84cdd6f7dd2012.js"
+  ],
+  "/_error": [
+    "static/chunks/305948d91f647ecc.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/App",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/305948d91f647ecc.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/b745f502ed22cb52.js",
+  "static/chunks/7df317975d29ae3e.js",
+  "static/chunks/turbopack-bac44c4f66823658.js"
+])
